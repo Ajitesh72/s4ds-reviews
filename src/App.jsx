a630f40177a3ec76b722c4530fea1f3c5c05ddf6
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-03-16 01:29:53 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
-import { useState,useEffect } from 'react'
-// import './App.css'
-import Papa from 'papaparse'; // Assuming you're using PapaParse for CSV parsing
-
+import "./App.css";
+import json from "./A98DB973KW.json";
 function App() {
-  const [reviews, setReviews] = useState([]);
-
-  useEffect(() => {
-    // Fetch or load your CSV file here
-    fetch('../src/assets/reviews.csv')
-      .then(response => response.text())
-      .then(csvData => {
-        // Parse CSV data
-        const parsedData = Papa.parse(csvData, { header: true });
-        // Extract user reviews
-        const userReviews = parsedData.data.map(row => row.reviews);
-        setReviews(userReviews);
-      })
-      .catch(error => {
-        console.error('Error fetching CSV file:', error);
-      });
-  }, []);
-
   return (
     <div>
-      <h1>User Reviews</h1>
-      <div>
-        {reviews.map((review, index) => (
-          <div key={index} dangerouslySetInnerHTML={{ __html: review }} />
-        ))}
-      </div>
+      <p>User Reviews</p>
+      {json?.map((eachObj, index) => (
+        <div
+          key={index}
+          dangerouslySetInnerHTML={{ __html: eachObj.reviews }}
+        />
+      ))}
     </div>
   );
 }
 
-export default App
+export default App;
